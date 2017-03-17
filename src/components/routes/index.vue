7bae0096fcac9d4fc802e3a1213c1d5d8b99b9b8
timofey <timoxa17@gmail.com>
2017-03-17 10:46:03 +0300

    minor changes

--- src/components/routes/index.vue
+++ src/components/routes/index.vue
@@ -9,7 +9,9 @@
 	article
 		h1 Home page
 		p Lorem ipsum dolor sit amet, consectetur adipisicing elit. A alias autem cupiditate deleniti dolor esse exercitationem, fugit hic impedit inventore laudantium necessitatibus obcaecati odit perspiciatis quas quos sequi vitae. Excepturi.
-		p {{ "I AM LOWERCASED" | lower }}
-		p {{ "i am uppercased" | upper }}
+
+		p {{ "I AM LOWERCASED" | lower }} - `lower` filter
+		p {{ "i am uppercased" | upper }} - `upper` filter
 		input(v-focus placeholder="I am focused")
+		!=' - `focus` directive'
 </template>
