348053ac8ded067bb4c47f5aaf41d2cf75113575
timofey <timoxa17@gmail.com>
2017-05-12 17:52:14 +0300

    prevent circular error for axios JSON.stringify(error)

--- src/http.js
+++ src/http.js
@@ -1,8 +1,24 @@
 /* eslint-disable quote-props */
 import Axios from 'axios';
-export default Axios.create({
+
+const http = Axios.create({
 	headers: {
 		'X-Requested-With': 'XMLHttpRequest',
 		'Accept': 'application/json'
 	}
 });
+
+http.interceptors.response.use(null, baseErr => {
+	const err = {};
+	// prevent circular stringify error
+	if (baseErr.response) {
+		delete baseErr.response.request;
+		delete baseErr.response.config;
+	}
+	err.message = baseErr.message;
+	err.stack = baseErr.stack;
+	err.response = baseErr.response;
+	return Promise.reject(err);
+});
+
+export default http;
