edb82a08e6c20215804de4fd37b7244c5798d4fe
Timofey <timoxa17@gmail.com>
2017-06-12 15:53:59 +0300

    prefetch data example

--- src/components/routes/examples/async-item.vue
+++ src/components/routes/examples/async-item.vue
@@ -1,9 +1,22 @@
 <script>
 	export default {
 		routePath: ':id',
-		props: ['id']
+		props: ['id'],
+		data: () => ({
+			name: 'loading...',
+			description: 'loading...'
+		}),
+		prefetch: ({ props }) => new Promise(resolve => setTimeout(() => {
+			resolve({
+				name: 'Name of ' + props.id,
+				description: 'something'
+			});
+		}, 200))
 	};
 </script>
 <template lang="pug">
-	h1 Item # {{ id }}
+	article
+		h1 Item # {{ id }}
+		h2 {{ name }}
+		p {{ description }}
 </template>
