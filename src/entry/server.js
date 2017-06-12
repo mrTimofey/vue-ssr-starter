38d7524f544937953783ef15a429509061c818d5
Timofey <timoxa17@gmail.com>
2017-06-12 15:56:21 +0300

    prefetch data support

--- src/entry/server.js
+++ src/entry/server.js
@@ -1,6 +1,11 @@
 import createApp from './app';
 import http from 'src/http';
 
+function init(app, context) {
+	context.meta = app.$meta();
+	context.initialVuexState = app.$store.state;
+}
+
 export default context => {
 	const app = createApp(context);
 
@@ -12,6 +17,7 @@ export default context => {
 		app.$router.onReady(() => {
 			// router matched components with prefetch
 			const comps = app.$router.getMatchedComponents().filter(comp => typeof comp.prefetch === 'function');
+
 			// prefetch call arguments
 			const args = {
 				store: app.$store,
@@ -19,35 +25,34 @@ export default context => {
 				route: app.$route
 			};
 
-			const prefetches = comps.map(comp => {
-				const data = comp.prefetch(args);
-				comp.prefetchedData = data;
-				return data;
-			});
+			const prefetches = comps.map(comp => comp.prefetch(args));
 
 			if (typeof app.$options.prefetch === 'function') prefetches.push(app.$options.prefetch({
 				store: app.$store,
 				route: app.$route
 			}));
 
-			Promise.all(prefetches)
+			if (prefetches.length) Promise.all(prefetches)
 				.then(compData => {
-					context.meta = app.$meta();
-					context.initialVuexState = app.$store.state;
+					init(app, context);
 					// do not include root component here
 					context.initialComponentStates = compData.slice(0, comps.length);
+					for (let i in comps) comps[i].prefetchedData = context.initialComponentStates[i];
 					if (app.$route.meta && app.$route.meta.statusCode) context.statusCode = app.$route.meta.statusCode;
 					else if (app.$store.getters.serverError) context.statusCode = 500;
 					resolve(app);
 				})
 				.catch(err => {
 					// let the application deal with errors
-					context.meta = app.$meta();
 					app.$store.commit('fireServerError', err);
-					context.initialStoreState = app.$store.state;
+					init(app, context);
 					context.statusCode = 500;
 					resolve(app);
 				});
+			else {
+				init(app, context);
+				resolve(app);
+			}
 		});
 		app.$router.push(context.url);
 	});
