0f52860adeceab71d975e91fd62475b5b3be1a24
Timofey <timoxa17@gmail.com>
2017-06-17 18:31:49 +0300

    routing improvements

--- src/router.js
+++ src/router.js
@@ -9,19 +9,26 @@ Vue.use(Router);
 // register all components in directory as routes (excepting files/folders starting from "_")
 const requirePage = require.context('src/components/routes/', true, /^(?:(?!\/?_).)+\.(vue|js)$/);
 const routes = [];
-let route404;
+let route404, pathMap;
+
+if (process.env.NODE_ENV !== 'production') pathMap = {};
 
 for (let name of requirePage.keys()) {
-	let component = requirePage(name),
-		route = {
-			component,
-			// generate route path based on file path
-			path: name.substr(1, name.length - 5).replace(/\/index$/, '/') +
-				// allow components adding their own route parameters
-				(component.routePath ? ('/' + component.routePath) : ''),
-			// add meta fields if there are any
-			meta: component.routeMeta
-		};
+	let component = requirePage(name);
+	if (component.default) component = component.default;
+	const route = {
+		component,
+		// generate route path based on file path
+		path: name.substr(1)
+		// remove file extension
+		.replace(/\.[a-zA-Z0-9]+$/, '')
+		// remove /index
+		.replace(/\/index$/, '') +
+		// allow components adding their own route parameters
+		(component.routePath ? ('/' + component.routePath) : ''),
+		// add meta fields if there are any
+		meta: component.routeMeta
+	};
 
 	// prefetch data for all route components
 	if (!component.mixins) component.mixins = [];
@@ -37,6 +44,7 @@ for (let name of requirePage.keys()) {
 		route404 = route;
 	}
 	else {
+		if (route.path === '') route.path = '/';
 		// generate component name automatically
 		if (!component.name) component.name = filenameToCamelCase(name);
 
@@ -44,6 +52,12 @@ for (let name of requirePage.keys()) {
 		if (component.routes) route.children = component.routes;
 		// map route parameters to component props by default
 		route.props = component.routeProps === undefined ? true : component.routeProps;
+
+		if (process.env.NODE_ENV !== 'production') {
+			if (pathMap[route.path])
+				throw new Error(`Duplicate path in vue router: ${route.path}, components are: ${pathMap[route.path]}, ${name}`);
+			pathMap[route.path] = name;
+		}
 		routes.push(route);
 	}
 }
